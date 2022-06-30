bebc76c129e85a951fe11f20b2c8881b8c034713
Ranieri Teixeira <ranibtx@gmail.com>
2022-06-30 17:01:23 -0300

    use pt br

--- src/hellow.py
+++
@@ -1,4 +0,0 @@
-def hello():
-    """Print hello, world to demonstrate use of the source library."""
-    print("Hello, World")
-    return
---
+++ src/modulo.py
@@ -0,0 +1,4 @@
+def fn_exemplo():
+  """Imprime uma string para demonstrar o reuso de código em um notebook"""
+  print("Funciona!")
+  return
