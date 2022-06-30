348b32a61e2b8d410dd56ed54c9f0464de009fb2
Ranieri Teixeira <ranibtx@gmail.com>
2022-06-30 16:24:58 -0300

    reuse

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'colabore-msc',
-    packages = ['colabore-msc'],
-    package_dir = {'colabore-msc': 'src'}
+    name = 'reuse',
+    packages = ['reuse'],
+    package_dir = {'src'}
 )
